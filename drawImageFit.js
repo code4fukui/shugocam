2cdb55ddc13e96dc808701ad8ae2ef03994c581f
Taisuke Fukuno <fukuno@jig.jp>
2024-07-09 07:45:54 +0900

    Initial commit

---
+++ drawImageFit.js
@@ -0,0 +1,13 @@
+export const drawImageFit = (g, img, sw, sh, dx, dy, dw, dh) => {
+  const aspect = dh / dw;
+  const imgaspect = sh / sw;
+  if (imgaspect > aspect) {
+    const h = sw * aspect;
+    const y = (sh - h) / 2;
+    g.drawImage(img, 0, y, sw, h, dx, dy, dw, dh);
+  } else {
+    const w = vh / aspect;
+    const x = (sw - w) / 2;
+    g.drawImage(img, x, 0, w, sh, dx, dy, dw, dh);
+  }
+};
